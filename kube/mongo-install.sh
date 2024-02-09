2f69163f9cf6b9716eab12d081288288aa3e3def
D. Elmo Peele <Elmo.Peele@gmail.com>
2024-02-09 13:06:22 -0500

    Add documentation and use the proper name.

--- kube/mongo-install.sh
+++ kube/mongo-install.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 #
-#helm install mongodb oci://registry-1.docker.io/bitnamicharts/mongodb
-#   ^^ core cumps
+# :XXX: Seems v5+ of mongo requires a CPU with AVX support
 #
-#helm repo add bitnami https://charts.bitnami.com/bitnami
+#       Since our CPU doesn't seem to have AVX, we need to fall-back to the
+#       latest image prior to v5 -- 4.2.21-debian-10-r8
 #
-#helm search repo mongo -l
-#helm install mongodb bitnami/mongodb --version 13.18.5
-#   ^^ core dumps
-
-#helm install mongodb bitnami/mongodb --version 12.1.16
-#   ^^ core dumps
-
-# Seems v5+ of mongo requires a CPU with AVX support
-#helm install mongodb bitnami/mongodb \
-#  --set image.tag=4.2.21-debian-10-r8
-
+# We also create an 'ayia' user and database.
 ###
 helm install mongodb oci://registry-1.docker.io/bitnamicharts/mongodb \
   --set image.tag=4.2.21-debian-10-r8 \
-  --set 'auth.usernames={aiya}' \
-  --set 'auth.passwords={aiya}' \
-  --set 'auth.databases={aiya}'
+  --set 'auth.usernames={ayia}' \
+  --set 'auth.passwords={ayia}' \
+  --set 'auth.databases={ayia}'
