5b9206a33e5c69ac92b73fe4bd99973d6e799fd3
D. Elmo Peele <Elmo.Peele@gmail.com>
2024-03-01 09:52:47 -0500

    Add support for 'type' to indicate whether the note is a cross-reference (xref) or footnote (foot).

--- web-ui/src/lib/VerseNote.svelte
+++ web-ui/src/lib/VerseNote.svelte
@@ -1,18 +1,25 @@
 <script>
-  import { Popover }          from 'flowbite-svelte';
-  import { AnnotationSolid }  from 'flowbite-svelte-icons';
+  import { Popover }  from 'flowbite-svelte';
+
+  import { AnnotationSolid, LinkSolid } from 'flowbite-svelte-icons';
 
   /* Incoming properties:
    *  - id      The id of the note {String};
    *  - label   The note label {String};
+   *  - type    The note type (xref | foot) {String}
    */
   export let id;
   export let label;
+  export let type;
 </script>
 
-<div class='note inline-block'>
+<div class='note {type || 'foot'}'>
   <sup>
+   {#if type === 'xref'}
+    <LinkSolid id={ id } class='note label' title={ label } />
+   {:else}
     <AnnotationSolid id={ id } class='note label' title={ label } />
+   {/if}
   </sup>
   <Popover triggeredBy='#{id}' class='note content'>
     <slot />
