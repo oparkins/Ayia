35a7d685af8b529fb70d1117ab2f767b80461c6b
D. Elmo Peele <Elmo.Peele@gmail.com>
2024-03-08 16:06:31 -0500

    Include application version in the sveltekig config to make it available via  $service-worker{version} or $app/environment{version}

--- web-ui/svelte.config.js
+++ web-ui/svelte.config.js
@@ -1,7 +1,22 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from "@sveltejs/adapter-node";
-import preprocess from "svelte-preprocess";
+import adapter    from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
+
+// Read package.json for access to the app version {
+// import w/assert is currently experimental {
+//import pkg from './package.json' assert { type: 'json' };
+// import w/assert is currently experimental }
+
+import { readFileSync }   from 'fs';
+import { fileURLToPath }  from 'url';
+
+const file  = fileURLToPath( new URL('package.json', import.meta.url) );
+const json  = readFileSync( file, 'utf8' );
+const pkg   = JSON.parse( json );
+// Read package.json } */
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -19,6 +34,9 @@ const config = {
      * See https://kit.svelte.dev/docs/adapters for more information about
      * adapters.
      */
+    version: {
+      name: pkg.version,
+    },
 
     // We have changed this to point to a build directory
     // adapter: adapter()
