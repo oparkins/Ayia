3fa5be4a6d3688a8938b6ff09bad2343e707408c
D. Elmo Peele <Elmo.Peele@gmail.com>
2024-03-24 08:22:03 -0400

    Capture version 1 yvers rendering.

--- web-ui/src/lib/render/yvers.js
+++ web-ui/src/lib/render/yvers.js
@@ -1,3 +1,8 @@
+/****************************************************************************
+ * Version 2 {
+ *
+ */
+
 /**
  *  Type-specific rendering engines.
  *
@@ -6,7 +11,6 @@ import {
   html      as generate_note,
   activate  as activate_notes,
 } from '$lib/verse_note';
-
 
 /**
  *  Generate the HTML markup for an entire chapter.
@@ -476,3 +480,207 @@ export function html_char( state, item ) {
 
   return html.join(' ');
 }
+
+/* Version 2 }
+ ****************************************************************************
+ * Version 1 {
+ *
+ */
+
+/**
+ *  Render raw HTML for the given item key/value where the value is an array or
+ *  string (v1).
+ *
+ *  @method html_raw
+ *  @param  key The markup item key {String};
+ *  @param  val The markup item value {String | Array};
+ *
+ *  Example:
+ *    key:  'p'
+ *    val:  "Jesus answered him, "
+ *
+ *    html:
+ *      <span class='p'>Jesus answered him, </span>
+ *
+ *  Example:
+ *    key: 'wj'
+ *    val: [
+ *            "“I assure you ",
+ *            { it: "and" },
+ *            " most solemnly say to you, unless a person is born again"
+ *         ]
+ *
+ *    html:
+ *      <span class='wj'>
+ *        “I assure you
+ *        <i>and</i>
+ *        most solemnly say to you, unless a person is born again
+ *      </span>
+ *
+ *  @return The rendered HTML {String};
+ */
+export function html_raw( key, val ) {
+  const css   = (typeof(key) === 'string' && key.length > 0
+                  ? `class='${key}'`
+                  : '');
+  const html  = [
+    `<span ${css}>`,
+  ];
+  if (key === 'fq' || key === 'fqa') {
+    // fq : Quotation from current scripture
+    // fqa: Alternative Translation
+    html.push( ` “` );
+  }
+
+  if (Array.isArray(val)) {
+    // Flatten this array
+    val.forEach( el => {
+      if (typeof(el) === 'string') {
+        html.push( el );
+        return;
+      }
+
+      // Assume this is an object
+      const i_key = Object.keys(el)[0];
+      const i_val = el[i_key];
+
+      switch( i_key ) {
+        case 'it':  // italic
+          html.push( `<i>${i_val}</i>` );
+          break;
+
+        default:
+          html.push( `<span class='${i_key}'>${i_val}</span>` );
+          break;
+      }
+    });
+
+  } else {
+    html.push( val );
+
+  }
+
+  if (key === 'fq' || key === 'fqa') {
+    // fq : Quotation from current scripture
+    // fqa: Alternative Translation
+    html.push( `” ` );
+  }
+  html.push('</span>');
+
+  return html.join('');
+}
+
+/**
+ *  Generate the HTML markup information needed to render the given markup.
+ *
+ *  @method html_markup
+ *  @param  markup          The target markup element {Object};
+ *  @param  m_dex           The index of this markup element {Number};
+ *  @param  show            Show configuration (from state) {Object};
+ *  @param  show.footnotes  Show footnotes {State};
+ *  @param  show.xrefs      Show cross-references {State};
+ *  @param  show.redletters Show red-letters {State};
+ *
+ *  @return HTML markup information {Object | Array};
+ *            { tag, raw, component, props }
+ */
+function html_markup( markup, m_dex, show={} ) {
+  const m_key   = Object.keys(markup)[0];
+  const m_val   = markup[m_key];
+  const css     = `${m_key}`;
+  const html    = {
+    tag       : '@html',
+    raw       : `<span class='${css}'>${m_val}</span>`,
+
+    component : null,
+    props     : {},
+  };
+
+  switch( m_key ) {
+    case 'label':   // Verse label
+      html.raw  = `<sup class='verse ${css}'>${m_val}</sup>`;
+      break;
+
+    case 'b':       // Blank line
+      html.raw  = `<br />`;
+      break;
+
+    case 'br':      // Verse break
+      html.raw  = `<p class='${css} break' />`;
+      break;
+
+    case 'it':      // Italic
+      html.raw  = `<i class='${css}'>${m_val}</i>`;
+      break;
+
+    case 'sc':      // Small-cap text
+      html.raw  = `<span class='${css}'>${m_val}</span>`;
+      break;
+
+    case 'note.x':    // Cross-reference : fall-through
+      if (! show.xrefs ) {
+        return null;
+      }
+      // fall-through
+
+    case 'note.f': {  // Footnote
+      if (! show.footnotes && m_key === 'note.f') {
+        return null;
+      }
+
+      const id      = `${verse_ref.replaceAll('.','-')}-${m_dex}`;
+      const type    = (m_key.split('.').pop() === 'x'
+                        ? 'xref'
+                        : 'foot');
+      let   label   = '#';
+      const content = m_val.map( n_obj => {
+        if (typeof(n_obj) === 'string') {
+          return (type === 'xref'
+                    ? `<span class='xt'>${n_obj}</span>`
+                    : n_obj);
+        }
+
+        const i_key = Object.keys(n_obj)[0];
+        const i_val = n_obj[i_key];
+        if (i_key === 'label') {
+          label = i_val;
+          return;
+        }
+        return html_raw( i_key, i_val );
+
+      })
+      .filter( el => el != null )
+      .join('');
+
+      html.tag        = '@component';
+      html.component  = VerseNote;
+      html.props      = { id, label, type };
+      html.raw        = content;
+
+    } break;
+
+    default: {
+      const isSect  = (m_key[0] === 's');
+      const isRef   = (m_key[0] === 'r');
+
+      if ( isSect || isRef) {
+        // This is a section or reference
+        const render_val  = html_raw( null, m_val );
+        html.raw  = `<p class='${m_key[0]} ${m_key}'>${render_val}</p>`;
+
+      } else {
+        let render_key  = m_key;
+        if (! show.redletters && m_key === 'wj') {
+          // Hide the red-letters
+          render_key = 'p';
+        }
+
+        html.raw  = html_raw( render_key, m_val );
+      }
+    } break;
+  }
+
+  return html;
+}
+/* Version 1 }
+ ****************************************************************************/
