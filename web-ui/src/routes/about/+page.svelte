1f826425f4c034a450914a5fe306925875e6e4aa
D. Elmo Peele <Elmo.Peele@gmail.com>
2024-03-29 09:18:49 -0400

    Include the version along with additinal descriptives.

--- web-ui/src/routes/about/+page.svelte
+++ web-ui/src/routes/about/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-  import { Card } from 'flowbite-svelte';
+  import { version } from '$app/environment'
+
+  import { Card, Heading, Badge, P } from 'flowbite-svelte';
 </script>
 
 <svelte:head>
@@ -9,8 +11,18 @@
 
 <div class='flex flex-col w-full h-full py-4'>
   <Card size='md' class='bg-gray-100 dark:bg-gray-900 mx-auto h-full'>
-    <p class='font-normal'>
-      Ayia is a Bible study web application.
-    </p>
+    <Heading tag='h4' class='mb-4 flex items-center gap-4'>
+      Ayia <Badge>v{ version }</Badge>
+    </Heading>
+
+    <P class='mb-3'>
+      Ayia is a Bible study web application providing access to multiple
+      English versions, including the Interlinear Berean Standard Bible.
+    </P>
+
+    <P class='mb-3'>
+      The goal is to provide deeper insight into and understanding of God's
+      word and what He has spoken.
+    </P>
   </Card>
 </div>
