de6a5c5e9279c334fe1de994b53f042e26992dc5
D. Elmo Peele <Elmo.Peele@gmail.com>
2024-08-23 09:35:30 -0400

    Add verse selection and scroll to the base Chapter.

--- web-ui/src/lib/Chapter.svelte
+++ web-ui/src/lib/Chapter.svelte
@@ -6,6 +6,8 @@
    *  @element  Chapter
    *  @prop     is_loading    Indicates whether `content` is currently being
    *                          loaded {Boolean};
+   *  @prop     column        The column in which this chapter is presented
+   *                          (primary | column#) {String};
    *  @prop     version       The current version {Version};
    *  @prop     book          The target book {Book};
    *  @prop     verse         The target verse {VerseRef};
@@ -15,6 +17,7 @@
    *  External properties {
    */
   export let is_loading = true;
+  export let column     = null;   // The column for this chapter
   export let version    = null;   // The target version
   export let book       = null;   // The target book
   export let verse      = null;   // The target verse
@@ -25,6 +28,15 @@
    *  Imports {
    *
    */
+  import { afterUpdate }  from 'svelte';
+  import { get, derived } from 'svelte/store';
+
+  import {
+    version   as version_stores,
+    verse     as verse_store,
+    selected  as selected_store,
+  }  from '$lib/stores';
+
   import VerseText        from '$lib/VerseText.svelte';
   import VerseYvers       from '$lib/VerseYvers.svelte';
   import VerseInterlinear from '$lib/VerseInterlinear.svelte';
@@ -35,6 +47,12 @@
    */
   let container_el  = null;
   let verse_el      = VerseText;
+
+  const version_store = version_stores[ column ];
+  const is_selecting  = derived( selected_store, ( $selected_store ) => {
+    return (Array.isArray( $selected_store ) && $selected_store.length > 0);
+  });
+
 
   /**
    *  Update the component based upon `version.type`
@@ -65,6 +83,192 @@
     }
   }
 
+  /* As soon as this component has been updated, select and scroll to any
+   * target verse.
+   */
+  afterUpdate(async () => {
+    /*
+    console.log('Chapter.afterUpdate(): is_selecting[ %s : %s ], verse:',
+                String( $is_selecting ),
+                ($is_selecting ? $selected_store.join(', ') : 'null'),
+                verse );
+    // */
+
+    // New target verse, if a verse number was requested, select and scroll.
+    if (! $is_selecting) {
+      remove_selection();
+    }
+
+    const verse_nums  = ($is_selecting ? $selected_store : null);
+    if (Array.isArray( verse_nums ) && verse_nums.length > 0) {
+      // Locate all portions of all target verse(s)
+      const selector  = verse_nums.map( num => `[v="${num}"]` ).join(',');
+      const verses    = container_el.querySelectorAll( selector );
+      const first     = verses.item( 0 );
+
+      if (verses.length > 0) {
+        /*
+        console.log('Chapter.afterUpdate(): verse_nums,verses:',
+                    verse_nums, verses);
+        // */
+
+        // Select all portions of the target verse(s)
+        verse_nums.forEach( num => select_verse( num, verses ) );
+      }
+
+      if (first) {
+        /*
+        console.log('Chapter.afterUpdate(): scrollIntoView:', first);
+        // */
+
+        // Scroll the first of the target verse(s) into view
+        first.scrollIntoView({ behavior: 'auto', block: 'center'});
+
+      }
+    }
+  });
+
+  /**
+   *  Triggered whenever $verse_store changes, determine whether we are moving
+   *  to a new verse. If so, reset 'is_selecting'.
+   *
+   *  @method reset_selecting
+   *  @param  new_version The (new) version in `$version_store` {Version};
+   *  @param  new_verse   The (new) verse in `$verse_store` {Verse};
+   *
+   *  @return void
+   */
+  function reset_selecting( new_version, new_verse ) {
+
+    /*
+    console.log('reset_selecting(): version/new:', version, new_version);
+    console.log('reset_selecting(): verse/new  :', verse, new_verse);
+    console.log('reset_selecting(): is_selecting[ %s : %s ]',
+                String($is_selecting),
+                ($is_selecting ? $selected_store.join(', ') : 'null') );
+    // */
+
+    // Switching to a new version or verse so reset our local state
+    selected_store.set( null )
+
+    if (version !== new_version) {
+      version = new_version;
+    }
+    if (verse !== new_verse) {
+      verse = new_verse;
+    }
+
+    const verse_nums  = (verse && verse.verses);
+    if (Array.isArray( verse_nums ) && verse_nums.length > 0) {
+      // Update the set of selected verses
+      console.log('reset_selecting(): Update selected verses to:',
+                  verse_nums.join(', '));
+
+      selected_store.set( verse_nums );
+    }
+  }
+
+  /**
+   *  Select the numbered verse.
+   *
+   *  @method select_verse
+   *  @param  verse_num The verse number {Number};
+   *  @param  [verses]  The set of target verses ([v="${verse_num}"])
+   *                    {NodeList};
+   *
+   *  @return void
+   */
+  function select_verse( verse_num, verses=null ) {
+    const selected  = get( selected_store ) || [];
+    if (! selected.includes( verse_num )) {
+      selected.push( verse_num );
+    }
+
+    // Locate the nearest parent with a 'v' attribute
+    if (! (verses instanceof NodeList)) {
+      verses = container_el.querySelectorAll(`[v="${verse_num}"]`);
+    }
+
+    /*
+    console.log('Chapter.select_verse( %s ): [ %s ], %d elements ...',
+                verse_num,
+                selected.join(', '),
+                verses.length);
+    // */
+    verses.forEach( el => {
+      el.setAttribute( 'selected', 'true' );
+    });
+
+    selected_store.set( selected );
+  }
+
+  /**
+   *  Remove verse selection.
+   *
+   *  @method remove_selection
+   *
+   *  @return void
+   */
+  function remove_selection() {
+    const selected  = container_el.querySelectorAll('[selected="true"]');
+    selected.forEach( el => {
+      el.removeAttribute( 'selected' );
+    });
+
+    // Update 'is_selecting' (on the chapter container)
+    selected_store.set( null );
+  }
+
+  /**
+   *  Handle a click on a verse.
+   *
+   *  @method click_verse
+   *  @param  event     The triggering event {Event};
+   *
+   *  @return void
+   */
+  function click_verse( event ) {
+    const target  = event.target;
+
+    /* Do NOT respond to clicks within:
+     *  1. A note label;
+     *  2. Note content;
+     */
+    const isNoteLabel = target.closest('.note-label');
+    if (isNoteLabel != null) { return }
+
+    const isNoteContent = target.closest('.note-content');
+    if (isNoteContent != null) { return }
+
+    // Locate the nearest parent with a 'v' attribute
+    const el = event.target.closest('[v]');
+    if (el == null) { return }
+
+    // Identify the verse number and determine if the current verse is selected
+    const verse_num = el.getAttribute('v');
+    const select    = (! el.hasAttribute('selected'));
+
+    /*
+    console.log('Chapter.click_verse(): verse_num[ %s ], select[ %s ]:',
+                verse_num, String( select ), el);
+    // */
+
+    if (select) {
+      // Selet the target verse.
+      select_verse( verse_num );
+
+    } else {
+      // Remove verse selection
+      remove_selection();
+
+    }
+  }
+
+  /* Whenever `$version_store` or `$verse_store` change, trigger
+   * reset_selecting()
+   */
+  $: reset_selecting( $version_store, $verse_store );
+
   // When `version` changes, update the verse element
   $: update_el( version );
 
@@ -93,7 +297,11 @@
    *************************************************************************/
 </script>
 
-<div class='content { Css.content.join(' ') }' bind:this={container_el} >
+<div class='content { Css.content.join(' ') }'
+     selecting={ $is_selecting }
+      role='presentation'
+      on:click={ click_verse }
+      bind:this={container_el} >
   {#if is_loading}
     Loading { verse.ui_ref } ...
   {:else if content && content.verses}
