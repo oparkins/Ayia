610b7b571db325373e23a300c378b737457c92ee
D. Elmo Peele <Elmo.Peele@gmail.com>
2024-08-30 10:39:36 -0400

    Remove unused imports.

--- web-admin/tests/books-nameToABBR.js
+++ web-admin/tests/books-nameToABBR.js
@@ -1,8 +1,6 @@
-const Assert        = require('assert').strict;
-const Path          = require('path');
-const { read_csv }  = require('./lib/helpers');
-const Books         = require('../lib/books');
-const tests         = require('./fixtures/book-names');
+const Assert  = require('assert').strict;
+const Books   = require('../lib/books');
+const tests   = require('./fixtures/book-names');
 
 describe('books.nameToABBR', () => {
   tests.forEach( (test, idex) => {
