d862447ba6691291b3f26ff58ee03ec6c5d707f9
D. Elmo Peele <Elmo.Peele@gmail.com>
2026-04-30 07:03:23 -0400

    Update helpers to match new container image and configuration.

--- helm/etc/mongo-ayia.sh
+++ helm/etc/mongo-ayia.sh
@@ -2,6 +2,11 @@
 #
 DEPLOY='ayia-mongodb'
 
-kubectl exec -it deploy/$DEPLOY -- \
-  /bin/bash -c 'mongo --norc --username $MONGODB_EXTRA_USERNAMES --password $MONGODB_EXTRA_PASSWORDS $MONGODB_EXTRA_DATABASES'
+###
+# Bitnami
+#kubectl exec -it deploy/$DEPLOY -- \
+#  /bin/bash -c 'mongo --norc --username $MONGODB_EXTRA_USERNAMES --password $MONGODB_EXTRA_PASSWORDS $MONGODB_EXTRA_DATABASES'
 
+###
+kubectl exec -it deploy/$DEPLOY -- \
+  /bin/bash -c 'mongo --norc --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD $MONGO_INITDB_DATABASE'
--- helm/etc/mongo-db.sh
+++ helm/etc/mongo-db.sh
@@ -2,5 +2,11 @@
 #
 DEPLOY='ayia-mongodb'
 
+###
+# Bitnami
+#kubectl exec -it deploy/$DEPLOY -- \
+#  /bin/bash -c 'mongo --norc --username $MONGODB_ROOT_USER --password $MONGODB_ROOT_PASSWORD'
+
+###
 kubectl exec -it deploy/$DEPLOY -- \
-  /bin/bash -c 'mongo --norc --username $MONGODB_ROOT_USER --password $MONGODB_ROOT_PASSWORD'
+  /bin/bash -c 'mongo --norc --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD'
