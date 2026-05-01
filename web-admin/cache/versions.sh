567b0d85cb74cf7de37141da5783148adc3b766f
D. Elmo Peele <Elmo.Peele@gmail.com>
2026-05-01 08:22:15 -0400

    Add cache helpers and include the latest versions-eng.json with NRSV re-added.

---
+++ web-admin/cache/versions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# 
+YVERS_VERSIONS=(
+  'AMP'
+  'BSB'
+  'BSB-IL'
+  'CEV'
+  'CSB'
+  'ESV'
+  'GNT'
+  'HCSB'
+  'KJV'
+  'MEV'
+  'MSG'
+  'NASB1995'
+  'NASB2020'
+  'NCV'
+  'NIV11'
+  'NIV84'
+  'NKJV'
+  'NLT'
+  'NRSV'  # :NLA:
+  'RSV'
+)
+
+export YVERS_VERSIONS
