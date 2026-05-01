f47551a7165bea0bd6a583131982bd68d6f9c0f5
D. Elmo Peele <Elmo.Peele@gmail.com>
2026-05-01 17:47:42 -0400

    Connect directoy to the Ayia database using the admin database for authentication.

--- helm/etc/mongo-ayia.sh
+++ helm/etc/mongo-ayia.sh
@@ -8,5 +8,11 @@ DEPLOY='ayia-mongodb'
 #  /bin/bash -c 'mongo --norc --username $MONGODB_EXTRA_USERNAMES --password $MONGODB_EXTRA_PASSWORDS $MONGODB_EXTRA_DATABASES'
 
 ###
+# Base
+#kubectl exec -it deploy/$DEPLOY -- \
+#  /bin/bash -c 'mongo --norc --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD $MONGO_INITDB_DATABASE'
+
+###
+# Ayia
 kubectl exec -it deploy/$DEPLOY -- \
-  /bin/bash -c 'mongo --norc --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD $MONGO_INITDB_DATABASE'
+  /bin/bash -c 'mongo --norc --username $AYIA_USER_NAME --password $AYIA_USER_PASSWORD --authenticationDatabase admin $AYIA_DB_NAME'
